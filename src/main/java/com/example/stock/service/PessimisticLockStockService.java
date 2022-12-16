961645c3c5cca12e0e21dd81e7e08f7c19cbffb0
jeongyongwon <asdfg0237@golfzon.com>
2022-12-16 17:16:35 +0900

    pessimistic lock으로 데이터 정합성 보존 예제

---
+++ src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,25 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+//데이터의 정합성을 보장할 수는 있지만 성능이슈가 있음
+@Service
+public class PessimisticLockStockService {
+
+    private StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+        stock.decrease(quantity);
+        stockRepository.saveAndFlush(stock);
+
+    }
+}
